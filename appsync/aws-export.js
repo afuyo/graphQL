1a804a3f9bdfdf2b290fb27b585f2f3fa78387e4
afuyo <artmro@gmail.com>
2019-02-27 11:23:59 +0100

    aws export modification

--- appsync/aws-export.js
+++ appsync/aws-export.js
@@ -1,14 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
-    //"graphqlEndpoint": "https://tgxhdgaj2zamhadt64co53iuhy.appsync-api.us-east-1.amazonaws.com/graphql",
-    "graphqlEndpoint": "https://sisxo3n5mvaelo74i4hbkxwx5m.appsync-api.eu-central-1.amazonaws.com/graphql",
-   // "region": "us-east-1", //your region
-    "region": "eu-central-1", //your region
+    "graphqlEndpoint": "https://tgxhdgaj2zamhadt64co53iuhy.appsync-api.us-east-1.amazonaws.com/graphql",
+    //"graphqlEndpoint": "https://sisxo3n5mvaelo74i4hbkxwx5m.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "region": "us-east-1", //your region
+   // "region": "eu-central-1", //your region
     "authenticationType": "API_KEY",
     //"apiKey": "da2-ko4mkf4dbjcpppfzg2da2eydru",
-    "apiKey": "da2-nxlz2zs3fjcrhly5bncijp75by",
-    //"ApiId": "egywftpedne2ppgqh47xyij7iy"
-    "ApiId": "wvivyg2nqfgv7ilbvbw54tvddm"
+    "apiKey": "da2-jzvkre4senecnhaxckilfhjh4q",
+    "ApiId": "egywftpedne2ppgqh47xyij7iy"
+    //"ApiId": "wvivyg2nqfgv7ilbvbw54tvddm"
 };
 exports.default = config;
